d2586343ad7adaa213b5c7ad3609f246538c5174
ice x <ice2600x@gmail.com>
2024-09-09 14:14:51 +0300

    add .idea to .gitignore

---
+++ tsp_greedy.py
@@ -0,0 +1,21 @@
+def tsp_greedy(distance_matrix):
+    n = len(distance_matrix)
+    start = 0
+    unvisited = set(range(1, n))
+    path = [start]
+    while unvisited:
+        last = path[-1]
+        next_city = min(unvisited, key=lambda city: distance_matrix[last][city])
+        path.append(next_city)
+        unvisited.remove(next_city)
+    path.append(start)  # Возвращаемся в исходный город
+    return path
+
+# Пример использования
+distance_matrix = [
+    [0, 10, 15, 20],
+    [10, 0, 35, 25],
+    [15, 35, 0, 30],
+    [20, 25, 30, 0]
+]
+print(tsp_greedy(distance_matrix))
